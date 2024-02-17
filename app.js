c44b144db2edb63fa5090358d138716f812f60fa
dlsptm <dlsptm6981@gmail.com>
2024-02-17 19:39:47 +0100

    mailer

--- app.js
+++ app.js
@@ -1,45 +1,78 @@
 // import des dépendances nécessaire pour le projet
-const express = require('express');
+const express = require("express");
+require('dotenv').config();
+
 // middleware afin de charger les log
-const morgan = require('morgan');
+const morgan = require("morgan");
 const path = require("path");
-const favicon = require('serve-favicon');
+const favicon = require("serve-favicon");
 
+const sendMail = require('./service/mailer')
 
- // EJS (Embedded JavaScript Templating) = template qui permet de générer du balisage HTML en utilisant JavaScript.
-const ejs = require('ejs');
+// EJS (Embedded JavaScript Templating) = template qui permet de générer du balisage HTML en utilisant JavaScript.
+const ejs = require("ejs");
 
 // Initialisation de l'application Express
 const app = express();
 // Set le port 3000
-const port = 3000;
-const public = path.join(__dirname, 'public');
+const port = process.env.PORT || 3000;
+const public = path.join(__dirname, "public");
 
 app
-.use(morgan('dev'))
-// autotisation d'accéder au dossier public afin de charger les fichiers statics comme les img / fichiers s.css / js
-.use(express.static(public))
-// inclusion d'une image dans chaque onglet
-.use(favicon(__dirname + '/favicon.ico'))
-.use('/bootstrap-css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
-.use('/bootstrap-js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
+  .use(morgan("dev"))
+  // autotisation d'accéder au dossier public afin de charger les fichiers statics comme les img / fichiers s.css / js
+  .use(express.static(public))
+  // inclusion d'une image dans chaque onglet
+  .use(favicon(__dirname + "/favicon.ico"))
+  .use(
+    "/bootstrap-css",
+    express.static(__dirname + "/node_modules/bootstrap/dist/css")
+  )
+  .use(
+    "/bootstrap-js",
+    express.static(__dirname + "/node_modules/bootstrap/dist/js")
+  );
 
+// Middleware pour parser le corps de la requête
+app.use(express.urlencoded({ extended: true }));
 
 // utilisation des template js
 app.set("view-engine", "ejs");
 
 
 
-app.get('/', (req, res) => {
-  res.render(__dirname+"/views/index.ejs");
-})
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
+});
+
+// Route pour la soumission du formulaire
+app.post("/", async (req, res) => {
+  try {
+    const { firstname, lastname, email, message } = req.body;
+
+    await sendMail(
+      email,
+      process.env.EMAIL_USER,
+      `${firstname} ${lastname} vous a envoyé un message`,
+      message,
+
+       );
+
+    res.send("Email envoyé avec succès."); // Réponse à envoyer une fois l'e-mail envoyé avec succès
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Une erreur s'est produite lors de l'envoi de l'e-mail."); // Réponse en cas d'erreur
+  }
+});
 
 
-app.get('/category', (req, res) => {
-  res.render(__dirname+"/views/category/index.ejs");
-})
-
+app.get("/category", (req, res) => {
+  res.render(__dirname + "/views/category/index.ejs");
+});
 
 // on démarre le serveur
 // utilisation de nomdemon afin d'automatiser l'actualisation de la page en prenant compte des changements
-app.listen(port, () => console.log(`notre application Node est démarée sur http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`notre application Node est démarée sur http://localhost:${port}`)
+);
---
+++ service/mailer.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnAuthorized:true
+  }
+});
+
+transporter.verify((err, success) => {
+  if (err) console.error(err);
+  
+  if (success) console.log('Your config is correct');
+});
+
+function sendMail(to, from, subject, message, link ) {
+  let mailOptions;
+
+  if (link) {
+    mailOptions = {
+      from: from,
+      to: to,
+      subject: subject,
+      text: message,
+      html: `<p>${message}</p><a href="${link}">Cliquez ici pour plus de détails</a>`,
+    };
+  } else {
+    mailOptions = {
+      from: from,
+      to: to,
+      subject: subject,
+      text: message,
+      html: `<p>${message}</p>`,
+    };
+  }
+
+  transporter.sendMail(mailOptions);
+}
+
+module.exports = sendMail;
